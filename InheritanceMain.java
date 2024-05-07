04aeb0214b6ce3330a882c2d80398c0f8e1e55af
frking69 <121990793+frking69@users.noreply.github.com>
2024-05-07 18:26:45 +0700

    Create InheritanceMain.java

---
+++ InheritanceMain.java
@@ -0,0 +1,8 @@
+public class InheritanceMain {
+	    public static void main(String[] args) {
+	        Mahasiswa mhs = new Mahasiswa("223040081", "Muhammad Oding Kcadek", "Bandung");
+	        System.out.println("NRP: " + mhs.getNr());
+	        System.out.println("Nama: " + mhs.getNama());
+	        System.out.println("Alamat: " + mhs.getAlamat());
+	    }
+	}
